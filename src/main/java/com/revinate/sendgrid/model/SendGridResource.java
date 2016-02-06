b0b6e843747ef4b391aab74870c669852676cdd6
Alex Leigh <alex@revinate.com>
2016-02-06 11:14:18 -0800

    Use separate pretty-printing object mapper for converting objects to string.

--- src/main/java/com/revinate/sendgrid/model/SendGridResource.java
+++ src/main/java/com/revinate/sendgrid/model/SendGridResource.java
@@ -1,12 +1,20 @@
 package com.revinate.sendgrid.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.revinate.sendgrid.SendGrid;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.entity.EntityBuilder;
 
 public abstract class SendGridResource {
+
+    public static final ObjectMapper PRETTY_PRINT_OBJECT_MAPPER = new ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            .configure(SerializationFeature.INDENT_OUTPUT, true)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
     @JsonIgnore
     public abstract String getPathId();
@@ -26,7 +34,7 @@ public abstract class SendGridResource {
     @Override
     public String toString() {
         try {
-            return SendGrid.OBJECT_MAPPER.writeValueAsString(this);
+            return PRETTY_PRINT_OBJECT_MAPPER.writeValueAsString(this);
         } catch (JsonProcessingException e) {
             return super.toString();
         }
